b0486017ed11f84c4a358c422b45ca1bd065118f
Souchet Céline <celine.souchet@bonitasoft.com>
2022-08-24 17:26:27 +0200

    Refactor how to configure the buttons to avoid duplication code

---
+++ src/buttons.js
@@ -0,0 +1,34 @@
+import { hideHappyPath, showHappyPath } from "./happy-path";
+import { hideConformanceData, showConformanceData } from "./conformance";
+import { hideComplianceRules, showComplianceRules } from "./compliance-rules";
+
+const state = {
+    isHappyPathDisplayed : false,
+    isConformanceDisplayed : false,
+    isComplianceDisplayed : false,
+};
+
+/**
+ * @param {BpmnVisualization} bpmnVisualization
+ */
+export function configureButtons(bpmnVisualization) {
+    addEventListener("happy_path", state.isHappyPathDisplayed, () => showHappyPath(bpmnVisualization), () => hideHappyPath(bpmnVisualization));
+    addEventListener("conformance_data", state.isConformanceDisplayed, () => showConformanceData(bpmnVisualization), () => hideConformanceData(bpmnVisualization));
+    addEventListener("compliance_rules", state.isComplianceDisplayed, () => showComplianceRules(bpmnVisualization), () => hideComplianceRules());
+}
+
+function addEventListener(buttonId, buttonState, showCallback, hideCallback){
+    const button = document.getElementById(buttonId);
+
+    button.addEventListener("click", function () {
+        if (!buttonState) {
+            showCallback();
+            button.innerHTML = button.innerHTML.replace('Show', 'Hide');
+            buttonState = true;
+        } else {
+            hideCallback();
+            button.innerHTML = button.innerHTML.replace( 'Hide', 'Show');
+            buttonState = false;
+        }
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import { BpmnVisualization } from "bpmn-visualization";
 
 import diagramCollapsed from "./diagrams/EC-purchase-orders-collapsed.bpmn?raw";
-import { showHappyPath, hideHappyPath } from "./happy-path.js";
-import { showConformanceData, hideConformanceData } from "./conformance.js";
-import { showComplianceRules, hideComplianceRules } from "./compliance-rules.js";
-
-const state = {
-  isHappyPathDisplayed : false,
-  isConformanceDisplayed : false,
-  isComplianceDisplayed : false,
-};
+import { configureButtons } from "./buttons";
 
 // 'bpmn-visualization' API documentation: https://process-analytics.github.io/bpmn-visualization-js/api/index.html
 const bpmnVisualization = new BpmnVisualization({
@@ -26,45 +18,8 @@ footer.innerText = `bpmn-visualization@${version.lib}`;
 // Try the "Center" type to fit the whole container and center the diagram
 bpmnVisualization.load(diagramCollapsed, { fit: { type: "None" } });
 
-const happyPathButton = document.getElementById("happy_path");
-const conformanceButton = document.getElementById("conformance_data");
-const complianceButton = document.getElementById("compliance_rules");
+configureButtons(bpmnVisualization);
 
-happyPathButton.addEventListener("click", function () {
-  if (!state.isHappyPathDisplayed) {
-    showHappyPath(bpmnVisualization);
-    happyPathButton.innerHTML = "Hide happy path";
-    state.isHappyPathDisplayed = true;
-  } else {
-    hideHappyPath(bpmnVisualization);
-    happyPathButton.innerHTML = "Show happy path";
-    state.isHappyPathDisplayed = false;
-  }
-});
-
-conformanceButton.addEventListener("click", function () {
-  if (!state.isConformanceDisplayed) {
-    showConformanceData(bpmnVisualization);
-    conformanceButton.innerHTML = "Hide conformance data";
-    state.isConformanceDisplayed = true;
-  } else {
-    hideConformanceData(bpmnVisualization);
-    conformanceButton.innerHTML = "Show conformance data";
-    state.isConformanceDisplayed = false;
-  }
-});
-
-complianceButton.addEventListener("click", function () {
-  if (!state.isComplianceDisplayed) {
-    showComplianceRules(bpmnVisualization);
-    complianceButton.innerHTML = "Hide compliance rules";
-    state.isComplianceDisplayed = true;
-  } else {
-    hideComplianceRules();
-    complianceButton.innerHTML = "Show compliance rules";
-    state.isComplianceDisplayed = false;
-  }
-});
 
 /*TO BE IMPLEMENTED
 observe navigation and zooming and 
