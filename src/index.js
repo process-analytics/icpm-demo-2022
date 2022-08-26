04a293a50090155a67bb601eec7287d0a06eafb6
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2022-08-26 09:31:37 +0200

    [FEAT] Display subProcess (#13)
    - Add breadcrumb without style
    - Add secondary container for subprocess
    - Add interaction on subprocess for main container
    - Add interaction on the breadcrumb

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
 import { BpmnVisualization } from "bpmn-visualization";
 
-import diagramCollapsed from "./diagrams/EC-purchase-orders-collapsed.bpmn?raw";
+import collapsedDiagram from "./diagrams/EC-purchase-orders-collapsed.bpmn?raw";
 import { configureButtons } from "./buttons";
+import { loadBpmnDiagram } from "./diagram";
+
+// https://stackoverflow.com/questions/49338193/how-to-use-code-from-script-with-type-module
+window.loadBpmnDiagram = loadBpmnDiagram;
 
 // 'bpmn-visualization' API documentation: https://process-analytics.github.io/bpmn-visualization-js/api/index.html
-const bpmnVisualization = new BpmnVisualization({
-  container: "bpmn-container",
+const mainBpmnVisualization = new BpmnVisualization({
+  container: 'main-bpmn-container',
   navigation: { enabled: true } // remove this line or set to false if you don't want to use Diagram Navigation
 });
 
-// display the bpmn-visualization version in the footer
-const footer = document.querySelector("footer");
-const version = bpmnVisualization.getVersion();
-footer.innerText = `bpmn-visualization@${version.lib}`;
-
 // Load BPMN diagram
 // Try the "Center" type to fit the whole container and center the diagram
-bpmnVisualization.load(diagramCollapsed, { fit: { type: "None" } });
+mainBpmnVisualization.load(collapsedDiagram, { fit: { type: "None" } });
 
-configureButtons(bpmnVisualization);
+
+// Interaction
+let subProcessId = 'Activity_0ec8azh';
+mainBpmnVisualization.bpmnElementsRegistry.getElementsByIds(subProcessId)[0].htmlElement.onclick = () => {
+  loadBpmnDiagram('secondary');
+}
+mainBpmnVisualization.bpmnElementsRegistry.addCssClasses(subProcessId, 'c-hand');
+
+
+
+configureButtons(mainBpmnVisualization);
+
+// display the bpmn-visualization version in the footer
+const footer = document.querySelector("footer");
+const version = mainBpmnVisualization.getVersion();
+footer.innerText = `bpmn-visualization@${version.lib}`;
 
 
 /*TO BE IMPLEMENTED
 observe navigation and zooming and 
 update circle ripples and linearGradient if they were added*/
 
-var containerBpmn = document.querySelector("#bpmn-container");
+var containerBpmn = document.querySelector("#main-bpmn-container");
 
 var containerObserver = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
