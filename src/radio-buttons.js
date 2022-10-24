0b77dffc9de5c00a1c07c5b059d5f80543fbd2da
Souchet Céline <celine.souchet@bonitasoft.com>
2022-10-24 15:59:58 +0200

    refactor code for radio buttons

--- src/radio-buttons.js
+++ src/radio-buttons.js
@@ -2,49 +2,36 @@ import { hideHappyPath, showHappyPath } from "./happy-path";
 import { hideConformanceData, showConformanceData } from "./conformance";
 import { hideComplianceRules, showComplianceRules } from "./compliance-rules";
 
+let checkedKPIRadioButton = null;
+
 /**
  * @param {BpmnVisualization} bpmnVisualization
  */
 export function configureRadioButtons(bpmnVisualization) {
-    let happyPathButton = new KPIRadioButton("happy_path", () => showHappyPath(bpmnVisualization), () => hideHappyPath(bpmnVisualization));
-    let conformanceDataButton = new KPIRadioButton("conformance_data", () => showConformanceData(bpmnVisualization), () => hideConformanceData(bpmnVisualization));
-    let complianceRulesButton = new KPIRadioButton("compliance_rules", () => showComplianceRules(bpmnVisualization), () => hideComplianceRules(bpmnVisualization));
+    new KPIRadioButton("happy_path", () => showHappyPath(bpmnVisualization), () => hideHappyPath(bpmnVisualization));
+    new KPIRadioButton("conformance_data", () => showConformanceData(bpmnVisualization), () => hideConformanceData(bpmnVisualization));
+    new KPIRadioButton("compliance_rules", () => showComplianceRules(bpmnVisualization), () => hideComplianceRules(bpmnVisualization));
 
-    const hideAllCallback = () => {
-        happyPathButton.hide();
-        conformanceDataButton.hide();
-        complianceRulesButton.hide();
-    }
-
-    happyPathButton.addEventListenerOnClick(hideAllCallback);
-    conformanceDataButton.addEventListenerOnClick(hideAllCallback);
-    complianceRulesButton.addEventListenerOnClick(hideAllCallback);
-
-    document.getElementById("reset_all").addEventListener("click", hideAllCallback);
+    document.getElementById("reset_all").addEventListener("click", () => checkedKPIRadioButton?.hide());
 }
 
 class KPIRadioButton {
     constructor(id, showCallback, hideCallback) {
-        this.button = document.getElementById(id);
-        this.dataAreShowed = false;
-        this.showCallback = showCallback;
         this.hideCallback = hideCallback;
-    }
 
-    addEventListenerOnClick(hideAllCallback) {
-        this.button.addEventListener("click", () => {
-            if(!this.dataAreShowed) {
-                hideAllCallback();
-                this.showCallback();
-                this.dataAreShowed = true;
+        document.getElementById(id).addEventListener("click", function(){
+            if(checkedKPIRadioButton != this) {
+                checkedKPIRadioButton?.hide();
+                showCallback();
+                checkedKPIRadioButton = this;
             }
-        });
+        }.bind(this));
     }
 
     hide() {
-        if(this.dataAreShowed) {
+        if(checkedKPIRadioButton == this) {
             this.hideCallback();
-            this.dataAreShowed = false;
+            checkedKPIRadioButton = null;
         }
     }
 }
